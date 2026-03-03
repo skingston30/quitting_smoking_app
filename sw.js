ea7b6094dfa7f1ef07336ec73e1c96387dd193c5
skingston30 <skingston91@gmail.com>
2026-03-03 15:03:37 -0500

    another update

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
-const cacheName = 'freedom-v1';
-const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+const cacheName = 'freedom-v2'; // Changed to v2 to force an update
+const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
-// Install the service worker and cache files
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Fetch files from cache if offline
+// Add this to clear out old versions of the app
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
